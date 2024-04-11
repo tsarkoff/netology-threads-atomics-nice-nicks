de99bf2bc3e972394379e6818e553a45d78fa934
tsarkoff <tsarkov.vladimir@gmail.com>
2024-04-11 23:02:22 +0300

    Some minor Cosmetics & Formatting.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,53 +8,58 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
-    private static final int TEXTS_QUANTITY = 100_000;
-    private static final int COUNTERS_QUANTITY = 3;
-    private static final List<AtomicInteger> counters = new ArrayList<>(COUNTERS_QUANTITY);
+    private static final String TEXT_LETTERS = "abc";   // letters are used in nicknames
+    private static final int TEXTS_QUANTITY = 100_000;  // number of nicks are generated
+    private static final int COUNTERS_QUANTITY = 3;     // number of threads / variants of nicknames lengths
+    private static final List<AtomicInteger> counters = new ArrayList<>(COUNTERS_QUANTITY); // variable is to store nice nicknames number from Threads
 
+    // MAIN INIT & THREADS
     public static void main(String[] args) throws InterruptedException {
-        IntStream.range(0, COUNTERS_QUANTITY).forEach(i -> counters.add(new AtomicInteger()));
         String[] texts = generateTexts();
         ExecutorService executorService = Executors.newFixedThreadPool(COUNTERS_QUANTITY);
-
-        IntStream.range(0, COUNTERS_QUANTITY).forEach(i -> executorService.submit(() -> {
-            System.out.println("Thread " + i + " started.");
-            for (String text : texts) {
-                if (text.length() == i + COUNTERS_QUANTITY) {
-                    if (isNiceNick(text)) {
+        IntStream.range(0, COUNTERS_QUANTITY).forEach(i -> {
+            counters.add(new AtomicInteger());
+            executorService.submit(() -> {
+                System.out.println("Thread " + i + " started.");
+                for (String text : texts) {
+                    if (text.length() == i + COUNTERS_QUANTITY && isNiceNick(text)) {
                         counters.get(i).incrementAndGet();
                     }
                 }
-            }
-        }));
+            });
+        });
 
-        executorService.awaitTermination(1, TimeUnit.SECONDS);
+        if (!executorService.awaitTermination(1, TimeUnit.SECONDS)) {
+            System.out.println("executorService has not managed to await Termination by timeout ^ still wait for Threads finishing... ");
+        }
         executorService.shutdown();
-        IntStream.range(0, COUNTERS_QUANTITY).forEach(i -> System.out.printf("%nКрасивых слов с длиной %s: %s шт", i + COUNTERS_QUANTITY, counters.get(i)));
+        IntStream.range(0, COUNTERS_QUANTITY).forEach(i -> System.out.printf("Красивых слов с длиной %s: %s шт%n", i + COUNTERS_QUANTITY, counters.get(i)));
     }
 
-    // covered by @Test ( src -> test -> java -> MainTest.java )
+    // CHECK IF NICK IS NICE = covered by @Test ( src -> test -> java -> MainTest.java )
     public static boolean isNiceNick(String text) {
         return
-                new StringBuilder(text).reverse().toString().equals(text) // boolean isPolyandrous
-                        || text.chars().allMatch(ch -> ch == text.charAt(0)) // boolean isSingleChar
-                        || Stream.of(text.split("")).sorted().collect(Collectors.joining()).equals(text); // boolean isAscendingOrder
+                new StringBuilder(text).reverse().toString().equals(text)                                // boolean isPolyandrous
+                || text.chars().allMatch(ch -> ch == text.charAt(0))                                     // boolean isSingleChar
+                || Stream.of(text.split("")).sorted().collect(Collectors.joining()).equals(text); // boolean isAscendingOrder
     }
 
+    // GENERATE 100_000 NICKNAMES
     private static String[] generateTexts() {
         Random random = new Random();
         String[] texts = new String[TEXTS_QUANTITY];
         for (int i = 0; i < texts.length; i++) {
-            texts[i] = generateText("abc", 3 + random.nextInt(3));
+            texts[i] = generateText(COUNTERS_QUANTITY + random.nextInt(COUNTERS_QUANTITY));
         }
         return texts;
     }
 
-    private static String generateText(String letters, int length) {
+    // GENERATE 1 (one) NICKNAME
+    private static String generateText(int length) {
         Random random = new Random();
         StringBuilder text = new StringBuilder();
         for (int i = 0; i < length; i++) {
-            text.append(letters.charAt(random.nextInt(letters.length())));
+            text.append(TEXT_LETTERS.charAt(random.nextInt(TEXT_LETTERS.length())));
         }
         return text.toString();
     }
